917a6cf2d3cd66c71d25b1dfb378513dbfb64ad6
tonybalde <tonybalde@hotmail.com>
2022-08-08 17:01:52 -0300

    no text alert and autofocus first input text

--- index.js
+++ index.js
@@ -2,35 +2,48 @@
 function encriptar(){
     var texto = document.getElementById("inputTexto").value.toLowerCase();
 
-var txtCifrado = texto.replace(/e/igm, "enter");
-var txtCifrado = txtCifrado.replace(/o/igm, "ober");
-var txtCifrado = txtCifrado.replace(/i/igm, "imes");
-var txtCifrado = txtCifrado.replace(/a/igm, "ai");
-var txtCifrado = txtCifrado.replace(/u/igm, "ufat");
+    if (texto == "") { 
+  
+        alert("No has ingresado nada para encriptar");
+    
+    } else {
+        var txtCifrado = texto.replace(/e/igm, "enter");
+        var txtCifrado = txtCifrado.replace(/o/igm, "ober");
+        var txtCifrado = txtCifrado.replace(/i/igm, "imes");
+        var txtCifrado = txtCifrado.replace(/a/igm, "ai");
+        var txtCifrado = txtCifrado.replace(/u/igm, "ufat");
 
-document.getElementById("imgDer").style.display = "none";
-document.getElementById("texto").style.display = "none";
-document.getElementById("texto2").innerHTML = txtCifrado;
-document.getElementById("copiar").style.display = "show";
-document.getElementById("copiar").style.display = "inherit";
+        document.getElementById("imgDer").style.display = "none";
+        document.getElementById("texto").style.display = "none";
+        document.getElementById("texto2").innerHTML = txtCifrado;
+        document.getElementById("copiar").style.display = "show";
+        document.getElementById("copiar").style.display = "inherit";
+    }
 
 }
 
 function desencriptar(){
     var texto = document.getElementById("inputTexto").value.toLowerCase();
 
-var txtCifrado = texto.replace(/enter/igm, "e");
-var txtCifrado = txtCifrado.replace(/ober/igm, "o");
-var txtCifrado = txtCifrado.replace(/imes/igm, "i");
-var txtCifrado = txtCifrado.replace(/ai/igm, "a");
-var txtCifrado = txtCifrado.replace(/ufat/igm, "u");
+    if (texto == "") { 
+  
+        alert("No has ingresado nada para desencriptar");
+    
+    } else {
 
-document.getElementById("imgDer").style.display = "none";
-document.getElementById("texto").style.display = "none";
-document.getElementById("texto2").innerHTML = txtCifrado;
-document.getElementById("copiar").style.display = "show";
-document.getElementById("copiar").style.display = "inherit";
+        var txtCifrado = texto.replace(/enter/igm, "e");
+            var txtCifrado = txtCifrado.replace(/ober/igm, "o");
+        var txtCifrado = txtCifrado.replace(/imes/igm, "i");
+        var txtCifrado = txtCifrado.replace(/ai/igm, "a");
+        var txtCifrado = txtCifrado.replace(/ufat/igm, "u");
 
+        document.getElementById("imgDer").style.display = "none";
+        document.getElementById("texto").style.display = "none";
+        document.getElementById("texto2").innerHTML = txtCifrado;
+        document.getElementById("copiar").style.display = "show";
+        document.getElementById("copiar").style.display = "inherit";
+
+    }
 }
 
 function copiar(){
